22dc060fff63ab48fbdd6896a55e79c30d4d4aec
rajsekharsaha <rajs4809342@gmail.com>
2023-09-05 14:26:39 +0530

    ok

--- js/navbar.js
+++ js/navbar.js
@@ -5,9 +5,9 @@ import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged ,signOut } 
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
     apiKey: "AIzaSyDc0LPuRl1K6W6rBeZU_kOVsiPGDof4Gkg",
     authDomain: "best-8c0e6.firebaseapp.com",
@@ -68,7 +68,7 @@ document.getElementById('forgetpass').addEventListener('click', function (event)
 //feedback
 document.getElementById('feedback').addEventListener('click', function (event) {
     event.preventDefault();
-    window.location.href = "forgetpass.html";
+    window.location.href = "feedback.html";
 });
 
 //share
@@ -80,13 +80,13 @@ document.getElementById('share').addEventListener('click', function (event) {
 //about
 document.getElementById('about').addEventListener('click', function (event) {
     event.preventDefault();
-    window.location.href = "forgetpass.html";
+    window.location.href = "about.html";
 });
 
 //contact
 document.getElementById('contact').addEventListener('click', function (event) {
     event.preventDefault();
-    window.location.href = "forgetpass.html";
+    window.location.href = "contact.html";
 });
 
 // Logout function
